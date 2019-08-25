d3819b1dfd6f81b3fa4346d4a9a156a5c019d660
Joel N. Johnson <joel.johnson675@gmail.com>
2019-08-25 15:07:04 -0700

    Convert tabs to spaces

--- Zip/download.php
+++ Zip/download.php
@@ -4,52 +4,52 @@ files being downloaded from Corvin.
 
 Variables
 
-	conn							- MySQL database connection
-	userID						- user ID of the user; unique integer used to identify
-											the user and grab other info from their row in
-											the database
-	_SESSION[userID]	- global variable used to maintain user's identity across
-											pages
-	user							- array;
-												user[0] = user's first name,
-												user[1] = user's last name
-	currentPathString - sanitized query string from previous page and passed via
-											post data; used to return user to the directory they
-											were in before a download error occurred
-	recycleBin				- Used to determine if download request is for a file or
-											 folder in the recycle bin;
-												recycleBin = '0 - recycleBin' if it is,
-												recycleBin = '' if it isn't
-	downloadDirectory	-	string containing full path to the directory containing
-	 										the file or folder to be downloaded
-	fileToDownload		-	information about file to be downloaded, structured as
+  conn              - MySQL database connection
+  userID            - user ID of the user; unique integer used to identify
+                      the user and grab other info from their row in
+                      the database
+  _SESSION[userID]  - global variable used to maintain user's identity across
+                      pages
+  user              - array;
+                        user[0] = user's first name,
+                        user[1] = user's last name
+  currentPathString - sanitized query string from previous page and passed via
+                      post data; used to return user to the directory they
+                      were in before a download error occurred
+  recycleBin        - Used to determine if download request is for a file or
+                      folder in the recycle bin;
+                        recycleBin = '0 - recycleBin' if it is,
+                        recycleBin = '' if it isn't
+  downloadDirectory - string containing full path to the directory containing
+                      the file or folder to be downloaded
+  fileToDownload    - information about file to be downloaded, structured as
 
-											filter_input(
-												type of input,
-												name of variable to get,
-												FILTER_SANITIZE_STRING:
-												filter that removes tags and
-												removes or encodes special
-												characters from a string
-											)
+                        filter_input(
+                          type of input,
+                          name of variable to get,
+                          FILTER_SANITIZE_STRING:
+                          filter that removes tags and
+                          removes or encodes special
+                          characters from a string
+                        )
 
-	fullPath					-	full path of file to be downloaded including file name
-											and type
-	realPath					- strips and and all '../'s in fullPath
-	zipFileName				- assigns url-encoded file name to zip file to download
-	zip								- ZipArchive object; used for creating zip files of folder
-											to download
-	files							- array containing all files in the directory to zip; each
-											element is a string containing the real path to the file
-	name							- key for files array; name of file is used as index
-	file							- single file in array of files; string containing real
-											path to an individual file
-	filePath					- strips any and all '../'s from file
-	relativePath			- relative path of file; chops filePath string down to the
-											length of realPath + 1
+  fullPath          - full path of file to be downloaded including file name
+                      and type
+  realPath          - strips and and all '../'s in fullPath
+  zipFileName       - assigns url-encoded file name to zip file to download
+  zip               - ZipArchive object; used for creating zip files of folder
+                      to download
+  files             - array containing all files in the directory to zip; each
+                      element is a string containing the real path to the file
+  name              - key for files array; name of file is used as index
+  file              - single file in array of files; string containing real
+                      path to an individual file
+  filePath          - strips any and all '../'s from file
+  relativePath      - relative path of file; chops filePath string down to the
+                      length of realPath + 1
 
 Issues: rework zip section, it's object oriented and some parts don't make
-				sense
+  sense
 
 Coded by: Joel N. Johnson
 -->
@@ -69,7 +69,7 @@ $conn = mysqli_connect("127.0.0.1", "joel", "Daytona675");
 //Check if connected to MYSQLI server
 if (!$conn) {
   echo("Failed to connect to database: " . mysqli_connect_error()) .
-		"<br /><br />";
+  "<br /><br />";
 }
 
 //Go into Corvin database
@@ -82,8 +82,8 @@ $sql = "SELECT firstName, lastName FROM UserInfo WHERE id = '$userID'";
 $user = mysqli_fetch_array(mysqli_query($conn, $sql));
 
 $currentPathString = filter_input(
-	INPUT_POST,
-	"currentPathString",
+  INPUT_POST,
+  "currentPathString",
   FILTER_SANITIZE_STRING
 );
 
@@ -96,7 +96,7 @@ $downloadDirectory = "/../../../mnt/Raid1Array/Corvin/" . $recycleBin . "/" .
 
 //Assign name of file from input
 $fileToDownload = filter_input(
-	INPUT_POST,
+  INPUT_POST,
   "fileToDownload",
   FILTER_SANITIZE_STRING
 );
@@ -105,78 +105,78 @@ $fileToDownload = filter_input(
 $fullPath = $downloadDirectory . $fileToDownload;
 $realPath = realpath($fullPath);
 $zipfilename = $downloadDirectory . urlencode(basename($fileToDownload)) .
-	".zip";
+  ".zip";
 
 if (is_readable($fullPath)) {
-	if (is_dir($fullPath)) {
+  if (is_dir($fullPath)) {
 
-		// Initialize ZipArchive object
-		$zip = new ZipArchive();
-		$zip->open($zipfilename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+  // Initialize ZipArchive object
+  $zip = new ZipArchive();
+  $zip->open($zipfilename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-		// Create recursive directory iterator
-		$files = new RecursiveIteratorIterator(
-			new RecursiveDirectoryIterator($realPath),
+  // Create recursive directory iterator
+  $files = new RecursiveIteratorIterator(
+  new RecursiveDirectoryIterator($realPath),
       RecursiveIteratorIterator::LEAVES_ONLY
     );
 
-		foreach ($files as $name => $file) {
-			// Skip directories (they would be added automatically)
-			if (!$file->isDir()) {
-				// Get real and relative path for current file
-				$filePath = $file->getrealPath();
-				echo "filePath: " . $filePath . "<br>";
-				$relativePath = substr($filePath, strlen($realPath) + 1);
-				echo "relativePath: " . $relativePath . "<br>";
+  foreach ($files as $name => $file) {
+  // Skip directories (they would be added automatically)
+  if (!$file->isDir()) {
+  // Get real and relative path for current file
+  $filePath = $file->getrealPath();
+  echo "filePath: " . $filePath . "<br>";
+  $relativePath = substr($filePath, strlen($realPath) + 1);
+  echo "relativePath: " . $relativePath . "<br>";
 
-				// Add current file to archive
-				$zip->addFile($filePath, $relativePath);
-			}
-		}
+  // Add current file to archive
+  $zip->addFile($filePath, $relativePath);
+  }
+  }
 
-		// Zip archive will be created only after closing object
-		$zip->close();
+  // Zip archive will be created only after closing object
+  $zip->close();
 
-		//State headers
-		header("Content-Description: File Transfer");
-		header("Content-Type: application/octet-stream");
-		header("Content-Disposition: attachment; filename = '" .
-			basename($zipfilename) . "'");
-		header('Content-Transfer-Encoding: binary');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate, ' . 'post-check=0, pre-check=0');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($zipfilename));
-		ob_clean();
-		flush();
+  //State headers
+  header("Content-Description: File Transfer");
+  header("Content-Type: application/octet-stream");
+  header("Content-Disposition: attachment; filename = '" .
+  basename($zipfilename) . "'");
+  header('Content-Transfer-Encoding: binary');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate, ' . 'post-check=0, pre-check=0');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($zipfilename));
+  ob_clean();
+  flush();
 
-		//Force download of zip folder
-		readfile($zipfilename);
+  //Force download of zip folder
+  readfile($zipfilename);
 
-		unlink($zipfilename);
-		exit;
-	}
-	else {
-		//State headers
-		header("Content-Description: File Transfer");
-		header("Content-Type: application/octet-stream");
-		header('Content-Disposition: attachment; filename = "' .
-			basename($fullPath) . '"');
-		header('Content-Transfer-Encoding: binary');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate, ' . 'post-check=0, pre-check=0');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($fullPath));
-		ob_clean();
-		flush();
+  unlink($zipfilename);
+  exit;
+  }
+  else {
+  //State headers
+  header("Content-Description: File Transfer");
+  header("Content-Type: application/octet-stream");
+  header('Content-Disposition: attachment; filename = "' .
+  basename($fullPath) . '"');
+  header('Content-Transfer-Encoding: binary');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate, ' . 'post-check=0, pre-check=0');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($fullPath));
+  ob_clean();
+  flush();
 
-		//Force download of file
-		readfile($fullPath);
-		exit;
-	}
+  //Force download of file
+  readfile($fullPath);
+  exit;
+  }
 }
 else {
   echo "Download failed because the file does not exist in the current " .
-		"directory.";
+  "directory.";
 }
 ?>
