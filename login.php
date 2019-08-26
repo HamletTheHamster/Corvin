6f3e26a23c905da2a8f7d459a3b30c3163dc097f
Joel N. Johnson <joel.johnson675@gmail.com>
2019-08-25 18:19:50 -0700

    Formatting/Code Cleanup

--- login.php
+++ login.php
@@ -1,13 +1,11 @@
 <!--
-    This is the a php file for Corvin that acts as a redirect from validate.php
-    when the user has entered incorrect credentials and wishes to return to
-    cor.vin with a clean url.
+This is the a php file for Corvin that acts as a redirect from validate.php
+when the user has entered incorrect credentials and wishes to return to cor.vin
+with a clean url.
 
-    Last updated: January 8, 2019
-
-    Coded by: Joel N. Johnson
+Coded by: Joel N. Johnson
 -->
 
 <?php
-    header("Location: /");
+header("Location: /");
 ?>
--- logout.php
+++ logout.php
@@ -1,23 +1,17 @@
 <!--
-        This is a php file for Corvin which each user's main page calls when the
-        user clicks logout, thus ending the user's current session. This happens
-        automatically if they close their browser.
+This is a php file for Corvin which is called when the user clicks logout, thus
+ending the user's current session.
 
-        Last updated: January 8, 2019
-
-        Coded by: Joel N. Johnson
+Coded by: Joel N. Johnson
 -->
 
 <?php
-
 // Start session
 session_start();
 
 // Destroy all sessions
-if(session_destroy())
-{
-    // Redirect to index.html
+if(session_destroy()) {
+  // Redirect to index.html
 	header("Location: /");
 }
-
 ?>
