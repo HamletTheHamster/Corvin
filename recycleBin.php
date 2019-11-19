1af570600a4a0f67cd90a67613671b22cb22dd7d
Joel Johnson <joel.johnson675@gmail.com>
2019-11-19 13:57:59 -0700

    Top bar external javascript for all pages

--- recycleBin.php
+++ recycleBin.php
@@ -317,58 +317,7 @@ elseif (time() - $_SESSION['Created'] > 1200) {
   </div>
 </div>
 
-<script>
-// Workspaces
-function workspacesDropDownMenu() {
-  if (document.getElementById("workspacesMenuContent").classList.contains(o+"Show")) {
-    document.getElementById("workspacesButton").classList.remove(o+"Active");
-  }
-  else {
-    document.getElementById("workspacesButton").classList.add(o+"Active");
-    document.getElementById("accountMenuContent").classList.remove(o+"Show");
-    document.getElementById("accountButton").classList.remove(o+"Active");
-  }
-  document.getElementById("workspacesMenuContent").classList.toggle(o+"Show");
-}
-
-function createWorkspacePopup() {
-  document.getElementById("createWorkspacePopup").classList.toggle(o+"Show");
-  document.getElementById("newWorkspaceNameTextField").focus();
-}
-
-function cancelCreateWorkspace() {
-  document.getElementById("createWorkspacePopup").classList.toggle(o+"Show");
-}
-
-// Account
-function accountDropDownMenu() {
-  if (document.getElementById("accountMenuContent").classList.contains(o+"Show")) {
-    document.getElementById("accountButton").classList.remove(o+"Active");
-  }
-  else {
-    document.getElementById("accountButton").classList.add(o+"Active");
-    document.getElementById("workspacesMenuContent").classList.remove(o+"Show");
-    document.getElementById("workspacesButton").classList.remove(o+"Active");
-  }
-  document.getElementById("accountMenuContent").classList.toggle(o+"Show");
-}
-
-// Click Off
-window.onclick = function(event) {
-  if (document.getElementById("accountMenuContent").classList.contains(o+"Show")) {
-    if (!event.target.matches("."+o+"AccountButton")) {
-      document.getElementById("accountMenuContent").classList.remove(o+"Show");
-      document.getElementById("accountButton").classList.remove(o+"Active");
-    }
-  }
-  else if (document.getElementById("workspacesMenuContent").classList.contains(o+"Show")) {
-    if (!event.target.matches("."+o+"WorkspacesButton")) {
-      document.getElementById("workspacesMenuContent").classList.remove(o+"Show");
-      document.getElementById("workspacesButton").classList.remove(o+"Active");
-    }
-  }
-}
-</script>
+<script src = 'topBar.js'></script>
 
 <!-- 3 Main Content -->
 <div class = '<?php echo $o;?>MainContent'>
